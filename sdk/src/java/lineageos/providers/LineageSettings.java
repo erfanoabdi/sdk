3438771c2b882c9864a0faebe3ee839df8dca557
Erfan Abdi <erfangplus@gmail.com>
2023-02-02 06:29:46 +0330

    Add microg disabler service
    Change-Id: I03a8d4f8914946ec6671cad82e05c712d4dd2a5e

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -2098,6 +2098,15 @@ public final class LineageSettings {
                 sBooleanValidator;
 
         /**
+         * Whether to enable microg.
+         */
+        public static final String ENABLE_MICROG = "enable_microg";
+
+        /** @hide */
+        public static final Validator ENABLE_MICROG_VALIDATOR =
+                sBooleanValidator;
+
+        /**
          * I can haz more bukkits
          * @hide
          */
@@ -2274,6 +2283,8 @@ public final class LineageSettings {
                     CLICK_PARTIAL_SCREENSHOT_VALIDATOR);
             VALIDATORS.put(ENABLE_TASKBAR,
                     ENABLE_TASKBAR_VALIDATOR);
+            VALIDATORS.put(ENABLE_MICROG,
+                    ENABLE_MICROG_VALIDATOR);
             VALIDATORS.put(__MAGICAL_TEST_PASSING_ENABLER,
                     __MAGICAL_TEST_PASSING_ENABLER_VALIDATOR);
         };
